f64a17c717e773bdfae470a1684073b700a3a3bb
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2024-10-25 16:52:58 +0300

    updated

---
+++ index.js
@@ -0,0 +1,12 @@
+//Write a JavaScript program that manages student grades.
+//The program should: Prompt the user to input multiple student names and their corresponding marks (marks should be between 0 and 100).
+
+//If the input is invalid, display an error message and prompt the user to enter the mark again.
+//Assign grades based on the following criteria:
+//A: Marks greater than 79
+//B: Marks between 60 and 79 (inclusive)
+//C: Marks between 50 and 59 (inclusive)
+//D: Marks between 40 and 49 (inclusive)
+//E: Marks less than 40
+
+//Allow the user to end the input process by typing "done" instead of entering a student's name. Display the student names, their marks, and the corresponding grades.
