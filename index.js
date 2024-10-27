7c8a704a9124fea2935337643a05f16a43e58a77
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2024-10-28 00:09:44 +0300

    complete

--- index.js
+++ index.js
@@ -1,39 +1,74 @@
-function calculateNetSalary(basicSalary, benefits) {
-  
-    const taxRate = 0.2
-    const nhifRate = 0.1
-    const nssfRate = 0.2
-  
+const readline = require('readline');
+const rl =readline.createInterface({ 
+    input: process.stdin, output: process.stdout });
+rl.question('What is your salary? ', (answer) => 
+    { const salary = answer 
+        console.log(calculatePaye(salary))
+
+        console.log(calculateNssf(salary))
+        console.log(calculateNhif(grossPay))
+    rl.close(); 
+});
+function calculatePaye(salary) {
+
+    if (salary <= 24000) {
+        return salary * 0.1;
+    } else if (salary <= 32333) {
+        return salary * 0.25;
+    } else if (salary <= 500000) {
+        return salary * 0.3;
+    } else if (salary <= 800000) {
+        return salary * 0.325;
+    } else {
+         return salary *0.35;
+    }
+}
+
+
+function calculateNhif(grossPay) {
+    if (grossPay <= 5999) {
+        return 150;
+    } else if (grossPay <= 7999) {
+        return 300;
+    } else if (grossPay <= 11999) {
+        return 400;
+    } else if (grossPay <= 14999) {
+        return 500;
+    } else if (grossPay <= 19999) {
+        return 600;
+    } else if (grossPay <= 24999) {
+        return 750;
+    } else if (grossPay <= 29999) {
+        return 850;
+    } else if (grossPay <= 34999) {
+        return 900;
+    } else if (grossPay <= 39999) {
+        return 950;
+    } else if (grossPay <= 44999) {
+        return 1000;
+    } else if (grossPay <= 49999) {
+        return 1100;
+    } else if (grossPay <= 59999) {
+        return 1200;
+    } else if (grossPay <= 69999) {
+        return 1300;
+    } else if (grossPay <= 79999) {
+        return 1400;
+    } else if (grossPay <= 89999) {
+        return 1500;
+    } else if (grossPay <= 99999) {
+        return 1600;
+    } else {
+        return  1700;
+    }
+}
+function calculateNssf(salary){
+   return 0.06 * salary
+}
+
     
-    const grossSalary = basicSalary + benefits;
-  
     
-    const tax = grossSalary * taxRate;
-    const nhif = grossSalary * nhifRate;
-    const nssf = grossSalary * nssfRate;
-  
     
-    const netSalary = grossSalary - (tax + nhif + nssf);
   
-    return {
-      grossSalary,
-      tax,
-      nhif,
-      nssf,
-      netSalary
-    };
-  }
-  
-  
-  const basicSalary = 59000; 
-  const benefits =4500; 
-  const result = calculateNetSalary(basicSalary, benefits);
-  console.log("Net Salary Calculation Results:");
-  console.log("Gross Salary:", result.grossSalary);
-  console.log("Tax:", result.tax);
-  console.log("NHIF Deductions:", result.nhif);
-  console.log("NSSF Deductions:", result.nssf);
-  console.log("Net Salary:", result.netSalary);
-  
-  
+
     
