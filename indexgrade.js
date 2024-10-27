737615a3860ae67e1bdbace733857d80dabc0d5f
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2024-10-27 22:17:51 +0300

    keep

--- indexgrade.js
+++ indexgrade.js
@@ -1,4 +1,4 @@
-function calculateGrade(mark) {
+function assignGrade(mark) {
     if (mark > 79) {
       return 'A';
     } else if (mark >= 60) {
@@ -13,13 +13,13 @@ function calculateGrade(mark) {
   }
   
   function studentGradeGenerator() {
-    const mark = parseFloat(prompt('Enter the student mark (0-100):'));
+    const mark = Number(prompt('Enter the student mark (0-100):'));
   
     if (isNaN(mark) || mark < 0 || mark > 100) {
       return 'Invalid input. Please enter a valid mark between 0 and 100.';
     }
   
-    const grade = calculateGrade(mark);
+    const grade = assignGrade(mark);
     return `The student's grade is: ${grade}`;
   }
   
